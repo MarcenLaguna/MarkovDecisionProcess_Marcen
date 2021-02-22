eea716d32e3f40fd033d273ce023fdda16ac3fa8
MarcenLaguna <78277011+MarcenLaguna@users.noreply.github.com>
2021-02-22 08:22:07 -0500

    primera

---
+++ src/DiscreteWorld/Reward.py
@@ -0,0 +1,38 @@
+from abc import abstractmethod
+from DiscreteWorld.Space import Space
+
+
+class Reward:
+    """
+    Models the rewards through time.
+
+    Attributes
+    __________
+    A: set
+        Set of accions
+    S: set
+        Set of states
+    T: int
+        Time Horizon
+
+    Methods
+    _______
+    adm_A
+        Function that given a (time, state) tuple returns the set of admisible actions for that pair
+    Q
+        Function that given a (state, action) tuple returns the probability distribution of the next state
+    """
+    def __init__(self, space: Space):
+        self.space = space
+        self.A = space.A
+        self.S = space.S
+        self.adm_A = space.adm_A
+        self.Q = space.Q
+        self.T = space.T
+
+    @abstractmethod
+    def reward(self, t, state, action=None):
+        """
+        Abstract method that computes the reward for a given (time, state, action) triple
+        """
+        ...
---
+++ src/DiscreteWorld/Space.py
@@ -0,0 +1,46 @@
+from abc import abstractmethod
+
+
+class Space:
+    """
+    Abstract class for the space of an discrete MDP.
+
+    Attributes
+    _________
+    A: set
+        Set of accions
+    S: set
+        Set of states
+    T: int
+        Time Horizon
+
+    Methods
+    _______
+    adm_A
+        Function that given a (time, state) tuple returns the set of admisible actions for that pair
+    Q
+        Function that given a (state, action) tuple returns the probability distribution of the next state
+
+    """
+    def __init__(self, actions, states, time_horizon):
+        self.A = actions
+        self.S = states
+        self.T = time_horizon
+        self.adm_A = NotImplemented
+        self.Q = NotImplemented
+        self.build_admisible_actions()
+        self.build_kernel()
+
+    @abstractmethod
+    def build_admisible_actions(self):
+        """
+        Abstract method that builds the admisible actions function and stores it in self.adm_A
+        """
+        ...
+
+    @abstractmethod
+    def build_kernel(self):
+        """
+        Abstract method that builds the stochastic kernel and stores it in self.Q
+        """
+        ...
